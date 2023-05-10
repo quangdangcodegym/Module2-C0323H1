7dd84f5dffdf8e11d0048e540b4ad46902ec729c
quangdangcodegym <quang.dang@codegym.vn>
2023-05-10 13:45:50 +0700

    java buoi 2

--- hellojava/src/main/java/com/codegym/JavaBasic.java
+++ hellojava/src/main/java/com/codegym/JavaBasic.java
@@ -6,23 +6,75 @@ public class JavaBasic {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        /**
-        System.out.println("Nhập vào 1 số:");
-        int num = scanner.nextInt();
+        float avg = 8.5f;
 
-        int total  = 0;
-        for (int i = 1; i < num; i++) {
-            total += i;
+        System.out.println(getRank(avg));
+    }
+
+    public static String getRank(float avg) {
+        if (avg >= 8 && avg <= 10) {
+            return "Gioi";
+        } else if (avg >= 5 && avg < 8) {
+            return "Kha";
+        } else if (avg >= 0 && avg < 5) {
+            return "Trung binh";
+        }else {
+            return "Khong hop le";
         }
-        System.out.println("Tổng từ 1 đến " + num + " là " + total );
+    }
+    public static void convertCurrency() {
+        int rate = 23000;
+        int choice = -1;
+
+
+        boolean checkInputContinue = false;
+        do {
+            checkInputContinue = false;
+            System.out.println("Bạn muốn nhập tiền USD hay VND");
+            System.out.println("Chọn 1.Nhập USD");
+            System.out.println("Chọn 2. Nhập VND");
+            choice = scanner.nextInt();
+            if (choice != 1 && choice != 2) {
+                System.out.println("Nhập không đúng vui lòng nhập lại");
+                checkInputContinue = true;
+            }
+        } while (checkInputContinue);
+
+
+        System.out.println("Nhập vào số tiền USD: ");
+        double money = scanner.nextDouble();
+        double total = 0;
+
+        switch (choice) {
+            case 1:
+                total = money*23000;
+                System.out.printf("%s USD tuong đương với %.0f tiền VND", money, total);
+                break;
+            case 2:
+                total = money / 23000;
+                System.out.printf("%s VND tuong đương với %.3f tiền USD", money, total);
+
+        }
+
+    }
+    public static void basicJava1() {
+        /**
+         System.out.println("Nhập vào 1 số:");
+         int num = scanner.nextInt();
+
+         int total  = 0;
+         for (int i = 1; i < num; i++) {
+         total += i;
+         }
+         System.out.println("Tổng từ 1 đến " + num + " là " + total );
          **/
 
         /**
-        int age = -1;
-        do {
-            System.out.println("Nhap vao tuổi: ");
-            age = scanner.nextInt();
-        } while (age < 0);
+         int age = -1;
+         do {
+         System.out.println("Nhap vao tuổi: ");
+         age = scanner.nextInt();
+         } while (age < 0);
 
 
          int age = -1;
@@ -55,14 +107,37 @@ public class JavaBasic {
          default:
          System.out.println("Tháng ko hợp lệ");
          }
+
+         System.out.println("Nhap vao 1 so, kiểm tra xem có phaải là số nguyên tố ko ");
+         int num = scanner.nextInt();
+
+
+
+         // 8: số nguyên: byte, short, int, long
+         int a = 129;
          **/
 
-        System.out.println("Nhap vao 1 so, kiểm tra xem có phaải là số nguyên tố ko ");
-        int num = scanner.nextInt();
+        /*
 
+        function tenHam(num1, num2){
+            return giatri;   // co the co return hoac khong
+        }
+         */
+
+//        printOneToNumber(100);
+
+    }
+    public static void basicPrintf() {
+        System.out.printf("%.3f la so thap phan\n", 10.456789);
+        System.out.printf("%20s | %10s | %20s\n", "NAME", "AGE", "SDT");
+        System.out.printf("%20s | %10s | %-20s\n", "Quang Dang", 18, "0399578133");
+        System.out.printf("%20s | %10s | %-20s\n", "Quang Dangsssssss", 18, "03955555559578133");
+        System.out.printf("%20s | %10s | %-20s\n", "Quang Dang123", 18, "0399578133");
+    }
+    public static boolean isPrime(int num) {
         boolean flag = true;
         if (num < 2) {
-            System.out.println(num + "Không phải la so nguyen to");
+            flag = false;
         }
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
@@ -70,12 +145,33 @@ public class JavaBasic {
                 break;
             }
         }
-        if (flag == true) {
-            System.out.println(num + "Là số nguyên tố");
-        }else{
-            System.out.println(num + "Không phải là số nguyên tố");
+        return flag;
+    }
+
+    public static void printOneToNumber(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
         }
+    }
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
 
+    public static void aPlus() {
+        int a  = 5;
+        int b = a++ + ++a*3;
+        System.out.println(a);
+        System.out.println(b);
+        // Nhân a = 6, b= 23;
+        // Thắng: a = 7, b = 24;
+        // Hiếu a = 7, b = 23; Bảo
 
+        /**
+         int a = 1;
+         do {
+         ++a;
+         } while (a++ < 10);
+         System.out.println(a);
+         */
     }
 }
