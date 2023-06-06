93c55affb28001274147ceff10bf790c7dc0cf2d
quangdangcodegym <quang.dang@codegym.vn>
2023-06-06 10:07:23 +0700

    string regex

---
+++ hellojava/src/main/java/com/codegym/string_regex/StringMain.java
@@ -0,0 +1,19 @@
+package com.codegym.string_regex;
+
+public class StringMain {
+    public static void main(String[] args) {
+        String a1 = "Quang Dang";
+        String a2 = "Thien Nhan";
+        int num = a1.compareTo(a2);
+        System.out.println("Num: " + num);
+        System.out.println(a1.startsWith("Quang"));
+
+        // Dang Van Quang;
+        StringBuilder b1 = new StringBuilder("Dang"); // ['D','a','n', 'g']
+        b1.append(" Quang");    // [Dang Quang]
+//        b1.insert(4, "Van");
+        b1.insert(5, "Van ");
+
+        System.out.println(b1);
+    }
+}
